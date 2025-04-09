8fb63ae21ee7a971ecbb3bb55207694d10b770bf
iaarav03 <138971341+iaarav03@users.noreply.github.com>
2025-04-10 00:25:09 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+    NEXT_PUBLIC_SOCKET_PATH: process.env.NEXT_PUBLIC_SOCKET_PATH,
+    MONGODB_URI: process.env.MONGODB_URI,
+    CORS_ORIGIN: process.env.CORS_ORIGIN,
+    MONGO_URI: process.env.MONGO_URI,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   rules: {
     "@typescript-eslint/no-unused-vars": "off", // Disable globally
-    
   },
   images: {
     remotePatterns: [
